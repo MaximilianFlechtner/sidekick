58011e86f752d5de82105af5e12402ee76079f5e
leoafarias <leokcta@gmail.com>
2021-04-12 19:18:26 -0400

    Package screen stateless

--- lib/screens/packages_screen.dart
+++ lib/screens/packages_screen.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:sidekick/providers/packages.provider.dart';
 import 'package:sidekick/screens/packages_scenes/most_used_section.dart';
 
-class PackagesScreen extends HookWidget {
+class PackagesScreen extends StatelessWidget {
   const PackagesScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final packages = useProvider(packagesProvider);
-
     return DefaultTabController(
       length: 3,
       child: Scaffold(
